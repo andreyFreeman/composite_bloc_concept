c627042c0926958a89e8a74ef051a3cc1a35c3cd
Andrei K <andreyakladov@gmail.com>
2024-02-22 01:31:11 +0100

    BlocProvider used
    decrement added

--- lib/counter_component_increment.dart
+++ lib/counter_component_increment.dart
@@ -2,16 +2,16 @@ import 'package:composite_bloc/composite_bloc.dart';
 import 'package:composite_bloc_sample_app/counter_event.dart';
 import 'package:composite_bloc_sample_app/counter_state.dart';
 
-class ComponentIncrement extends BlocComponent<CounterState> {
+class ComponentIncrement<Event extends CounterEvent> extends BlocComponent<CounterState> {
   final int incrementor;
-  ComponentIncrement({this.incrementor = 1, int initialValue = 0}) {
+  ComponentIncrement({this.incrementor = 1}) {
     addDependency<ComponentRegisterEventHandler<CounterEvent, CounterState>>();
   }
 
   @override
   Future<void> onInitialize() async {
     getComponent<ComponentRegisterEventHandler<CounterEvent, CounterState>>()!
-    .register<IncrementEvent>((event, emit) => 
+    .register<Event>((event, emit) => 
     emit(CounterState(value: getComponent<ComponentBlocProvider<CounterEvent, CounterState>>()!.getBloc()!.state.value + incrementor)));
   }        
 }
--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:composite_bloc/composite_bloc.dart';
 import 'package:composite_bloc_sample_app/counter_event.dart';
 import 'package:composite_bloc_sample_app/counter_state.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'component_state_logger.dart';
 import 'counter_component_increment.dart';
@@ -20,7 +21,19 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: BlocProvider(
+        create: (context) => CompositeBloc<CounterEvent, CounterState>(
+          state: CounterState(), 
+          components: [
+            ComponentBlocProvider<CounterEvent, CounterState>(),
+            ComponentRegisterEventHandler<CounterEvent, CounterState>(),
+            ComponentIncrement<IncrementEvent>(),
+            ComponentIncrement<DecrementEvent>(incrementor: -1),
+            ComponentStateLogger()
+          ]
+        ),
+        child: const MyHomePage(title: 'Flutter Demo Home Page'),
+      ),
     );
   }
 }
@@ -35,52 +48,77 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  late CompositeBloc<CounterEvent, CounterState> _bloc;
-
-  @override
-  void initState() {
-    super.initState();
-    _bloc = CompositeBloc(state: CounterState(), components: [
-      ComponentBlocProvider<CounterEvent, CounterState>(),
-      ComponentRegisterEventHandler<CounterEvent, CounterState>(),
-      ComponentIncrement(),
-      ComponentStateLogger()
-    ]);
-    _bloc.stream.listen((state) {
-      setState(() {
-        _counter = state.value;
-      });
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+        title: const Text('Counter App'),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      body: BlocBuilder<CompositeBloc<CounterEvent, CounterState>, CounterState>(
+        builder: (context, state) {
+          return Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'Counter Value:',
+                ),
+                Text(
+                  '${state.value}',
+                  style: Theme.of(context).textTheme.headlineMedium,
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
+          );
+        },
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: () => _bloc.add(CounterEvent.increment()),
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: () =>
+                context.read<CompositeBloc<CounterEvent, CounterState>>().add(CounterEvent.increment()),
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          const SizedBox(height: 10),
+          FloatingActionButton(
+            onPressed: () =>
+                context.read<CompositeBloc<CounterEvent, CounterState>>().add(CounterEvent.decrement()),
+            tooltip: 'Decrement',
+            child: const Icon(Icons.remove),
+          ),
+        ],
       ),
     );
   }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return Scaffold(
+  //     appBar: AppBar(
+  //       backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+  //       title: Text(widget.title),
+  //     ),
+  //     body: Center(
+  //       child: Column(
+  //         mainAxisAlignment: MainAxisAlignment.center,
+  //         children: <Widget>[
+  //           const Text(
+  //             'You have pushed the button this many times:',
+  //           ),
+  //           Text(
+  //             '$_counter',
+  //             style: Theme.of(context).textTheme.headlineMedium,
+  //           ),
+  //         ],
+  //       ),
+  //     ),
+  //     floatingActionButton: FloatingActionButton(
+  //       onPressed: () => _bloc.add(CounterEvent.increment()),
+  //       tooltip: 'Increment',
+  //       child: const Icon(Icons.add),
+  //     ),
+  //   );
+  // }
 }
